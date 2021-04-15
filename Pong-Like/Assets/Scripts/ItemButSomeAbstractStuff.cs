2f9301d4cae3ba3569a859bdb4ac27daae0d89fb
An3s079 <An3sFerreira@gmail.com>
2021-04-14 20:10:24 -1000

    fixed another sloppy commit

--- Pong-Like/Assets/Scripts/ItemButSomeAbstractStuff.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class ItemButSomeAbstractStuff : MonoBehaviour
-{
-
-   public abstract void Pickup(Ball ball, Player playerPaddle);
-}
